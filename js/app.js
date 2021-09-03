7fc038f07e91438a57e57b8efe34fe7ad506301a
Eric <when.the.music.play@gmail.com>
2021-09-02 21:51:25 -0400

    Ajout easter egg

--- js/app.js
+++ js/app.js
@@ -25,6 +25,135 @@ docReady(async function() {
 
     search.onkeyup = async function(event) {
         let query = event.target.value;
+        if(query.toUpperCase() === "Jésus".toUpperCase()) {
+            tsParticles.load("tsparticles", {
+                backgroundMask: {
+                  enable: true,
+                  cover: {
+                    value: {
+                      r: 0,
+                      g: 0,
+                      b: 0
+                    }
+                  }
+                },
+                background: {
+                  image: "url('https://particles.js.org/images/background.jpg')",
+                  size: "100% 100%",
+                  repeat: "no-repeat"
+                },
+                fullScreen: {
+                  enable: true,
+                  zIndex: -1
+                },
+                particles: {
+                  color: {
+                    value: ["#1E00FF", "#FF0061", "#E1FF00", "#00FF9E"],
+                    animation: {
+                      enable: true,
+                      speed: 30
+                    }
+                  },
+                  move: {
+                    direction: "bottom",
+                    enable: true,
+                    outModes: {
+                      default: "out"
+                    },
+                    size: true,
+                    speed: {
+                      min: 1,
+                      max: 3
+                    }
+                  },
+                  number: {
+                    value: 700,
+                    density: {
+                      enable: true,
+                      area: 800
+                    }
+                  },
+                  opacity: {
+                    value: 1,
+                    animation: {
+                      enable: false,
+                      startValue: "max",
+                      destroy: "min",
+                      speed: 0.3,
+                      sync: true
+                    }
+                  },
+                  rotate: {
+                    value: {
+                      min: 0,
+                      max: 360
+                    },
+                    direction: "random",
+                    move: true,
+                    animation: {
+                      enable: true,
+                      speed: 60
+                    }
+                  },
+                  tilt: {
+                    direction: "random",
+                    enable: true,
+                    move: true,
+                    value: {
+                      min: 0,
+                      max: 360
+                    },
+                    animation: {
+                      enable: true,
+                      speed: 60
+                    }
+                  },
+                  shape: {
+                    type: ["circle", "square", "polygon"],
+                    options: {
+                      polygon: [
+                        {
+                          sides: 5
+                        },
+                        {
+                          sides: 6
+                        }
+                      ]
+                    }
+                  },
+                  size: {
+                    value: {
+                      min: 3,
+                      max: 5
+                    }
+                  },
+                  roll: {
+                    darken: {
+                      enable: true,
+                      value: 30
+                    },
+                    enlighten: {
+                      enable: true,
+                      value: 30
+                    },
+                    enable: true,
+                    speed: {
+                      min: 15,
+                      max: 25
+                    }
+                  },
+                  wobble: {
+                    distance: 30,
+                    enable: true,
+                    move: true,
+                    speed: {
+                      min: -15,
+                      max: 15
+                    }
+                  }
+                }
+              });              
+        }
         if (query.length > 0) {
             clients = await callApi('clients?q=' + query)
             updateClientList(clients);
@@ -66,7 +195,7 @@ function createClientDialog() {
 async function clientDialog(id) {
     let client
     if(id == 0) {
-        client = { "name": ""};
+        client = { "name": "", "depots": [] };
     } else {
         client = await callApi('clients/' + id + '?_embed=depots');
     }
@@ -75,6 +204,7 @@ async function clientDialog(id) {
     let modalBody = modal.querySelector('.modal-body');
     let name = modalBody.querySelector('.name');
     let nameInput = modal.querySelector('.name-input');
+    let depots = modal.querySelector('.depots');
 
     name.innerHTML = client.name;
     if (client.name == "") {
@@ -82,6 +212,12 @@ async function clientDialog(id) {
     } else {
         nameInput.style.display = "none";
     }
+
+    let depotsHtml = '';
+    client.depots.forEach(function(depot) {
+        depotsHtml += "<li class=\"list-group-item\">"+ depot.montant +"</li>"
+    });
+    depots.innerHTML = depotsHtml;
 
     modal.style.display = "block";
 }
