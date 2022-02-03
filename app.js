4793ad492e8973f6069fa1e4e5211f09c7315c46
Eric Turcotte <eric.turcotte4@usherbrooke.ca>
2022-02-02 22:29:42 -0500

    Ajout de l'importation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
+const {Client, Depot} = require("./api/models");
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,6 +28,36 @@ let port = 'passenger'
 if(args[0] == 'dev') {
     port = 3000
 }
+
+if(args[1] == 'import') {
+    var db = require('./db.json')
+
+    Promise.all(db.clients.map(client => {
+        Client.create({
+            id: client.id,
+            name: client.name,
+            id_user: 11
+        }).then(function(result) {
+            console.log(result)
+        })
+    }))
+
+    Promise.all(db.depots.map(depot => {
+        Depot.create({
+            id: depot.id,
+            id_user: 11,
+            id_client: depot.clientId,
+            date_depot: depot.dateDepot,
+            montant: depot.montant,
+            etat: depot.etat,
+            date_etat: depot.dateEtat == '' ? null : depot.dateEtat,
+            note: depot.note
+        }).then(function(result) {
+            console.log(result)
+        })
+    }))
+}
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port 3000`)
