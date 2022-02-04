d0590b5a3867665ad7065beb100c7f87551aa36c
Eric Turcotte <eric.turcotte4@usherbrooke.ca>
2022-02-04 13:37:44 -0500

    Valider l'existance du client avant de faire une action sur un dépôt

--- api/controllers/depot_controller.js
+++ api/controllers/depot_controller.js
@@ -6,115 +6,121 @@ const {
     v4: uuidv4,
 } = require('uuid');
 
+const isExistingClient = (id_client, id_user) => {
+    return Client.findByIdAndUserId(id_client, id_user).then(client => {
+        if(client.length) {
+            return client
+        } else {
+            throw createError({
+                status: NOT_FOUND,
+                message: 'Client not found'
+            })
+        }
+    })
+}
+
 const getClientDepots = (req, res, next) => {
     const id_client = req.params.id_client
+    const id_user = req.id_user
 
-    Client.findById(id_client)
+    isExistingClient(id_client, id_user)
         .then(function(client) {
-            if(client.length === 0) {
-                next(createError({
-                    status: NOT_FOUND,
-                    message: 'Client not found'
-                }))
-            } else {
-                Depot.findAllByIdClient(id_client)
-                    .then(function(depots) {
-                        res.json({
-                            ok: true,
-                            message: 'Client\'s depots found',
-                            depots,
-                            id_user: req.id_user
-                        })
+            Depot.findAllByIdClient(id_client)
+                .then(function(depots) {
+                    res.json({
+                        ok: true,
+                        message: 'Client\'s depots found',
+                        depots,
+                        id_user: req.id_user
                     })
-                    .catch(next)
-            }
+                })
+                .catch(next)
 
         }).catch(next)
 }
 
 const postClientDepot = (req, res, next) => {
     const id_client = req.params.id_client
+    const id_user = req.id_user
 
-    Client.findById(id_client)
+    isExistingClient(id_client, id_user)
         .then(function(client) {
-            if(client.length === 0) {
-                next(createError({
-                    status: NOT_FOUND,
-                    message: 'Client not found'
-                }))
-            } else {
-                const id = uuidv4();
-                const date_depot = req.body.date_depot
-                const montant = req.body.montant
-                const etat = req.body.etat
-                const note = req.body.note
+            const id = uuidv4();
+            const date_depot = req.body.date_depot
+            const montant = req.body.montant
+            const etat = req.body.etat
+            const note = req.body.note
 
-                Depot.create({
-                    id,
-                    id_user: req.id_user,
-                    id_client,
-                    date_depot,
-                    montant,
-                    etat,
-                    note
-                }).then(function(depots) {
-                    res.json({
-                        ok: true,
-                        message: 'Depot created',
-                        depots,
-                        id_user: req.id_user
-                    })
-                }).catch(next)
-            }
-
+            Depot.create({
+                id,
+                id_user: req.id_user,
+                id_client,
+                date_depot,
+                montant,
+                etat,
+                note
+            }).then(function(depots) {
+                res.json({
+                    ok: true,
+                    message: 'Depot created',
+                    depots,
+                    id_user: req.id_user
+                })
+            }).catch(next)
         }).catch(next)
 }
 
 const putClientDepot = (req, res, next) => {
     const id_client = req.params.id_client
-    // Find client first
+    const id_user = req.id_user
 
-    const id = req.params.id
-    // const id = req.body.id
-    const date_depot = req.body.date_depot
-    const montant = req.body.montant
-    const etat = req.body.etat
-    const note = req.body.note
+    isExistingClient(id_client, id_user)
+        .then(function(client){
+            const id = req.params.id
+            // const id = req.body.id
+            const date_depot = req.body.date_depot
+            const montant = req.body.montant
+            const etat = req.body.etat
+            const note = req.body.note
 
-    Depot.update(id, {
-        id_user: req.id_user,
-        id_client,
-        date_depot,
-        montant,
-        etat,
-        note
-    }).then(function(depots) {
-        res.json({
-            ok: true,
-            message: 'Depot modified',
-            depots,
-            id_user: req.id_user
-        })
-    }).catch(next)
-}
-
-const deleteClientDepot = (req, res, next) => {
-    const id_client = req.params.id_client
-    // Find client first
-
-    const id = req.params.id
-
-    if (req.id_user) {
-        Client.destroy(id)
-            .then(function (depots) {
+            Depot.update(id, {
+                id_user: req.id_user,
+                id_client,
+                date_depot,
+                montant,
+                etat,
+                note
+            }).then(function(depots) {
                 res.json({
                     ok: true,
-                    message: 'Depot deleted',
+                    message: 'Depot modified',
                     depots,
                     id_user: req.id_user
                 })
             }).catch(next)
-    }
+        })
+        .catch(next)
+}
+
+const deleteClientDepot = (req, res, next) => {
+    const id_client = req.params.id_client
+    const id_user = req.id_user
+
+    isExistingClient(id_client, id_user)
+        .then(function(client) {
+            const id = req.params.id
+
+            Client.destroy(id)
+                .then(function (depots) {
+                    res.json({
+                        ok: true,
+                        message: 'Depot deleted',
+                        depots,
+                        id_user: req.id_user
+                    })
+                }).catch(next)
+        })
+        .catch(next)
 }
 
 module.exports = {
